0c48c26d529f021c85395a6f54f5c10c9ed86fa8
“t3wuke00” <t3wuke00@students.oamk.fi>
2024-04-07 14:36:58 +0300

    registration+login

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const { SolidWallRouter} = require("./routes/SolidWall")
+const { userRouter} = require('./routes/user')
 
 
 const port = 3001
-
 const app = express()
 app.use(cors())
 
+
 app.use('/', SolidWallRouter)
+app.use('user',userRouter)
+
+
 
 app.listen(port,() =>{
     console.log(`Server is listening on port ${port}`)
